59d18ab82923bd9c5ec84581d2e9702a324891e4
srijan440 <srijan440@gmail.com>
2024-10-16 22:58:00 +0530

    Html flies

---
+++ awe2.jsx
@@ -0,0 +1,63 @@
+import './awe2.css'
+
+export default function Homepage() {
+  return (
+    <div className="homepage">
+      <div className="header">
+        <div className="world-peas">
+          World Peas
+        </div>
+        <div className="shop">
+          Shop
+        </div>
+        <div className="newstand">
+          Newstand
+        </div>
+        <div className="who-we-are">
+          Who we are
+        </div>
+        <div className="my-profile">
+          My profile
+        </div>
+        <div className="cart-button">
+          <span className="basket-3">
+            Basket (3)
+          </span>
+        </div>
+      </div>
+      <p className="were-farmers-purveyors-and-eaters-of-organically-grown-food">
+        <span className="were-farmers-purveyors-and-eaters-of-organically-grown-food-sub-0"></span><span className="were-farmers-purveyors-and-eaters-of-organically-grown-food-sub-1"></span><span className="were-farmers-purveyors-and-eaters-of-organically-grown-food-sub-0"></span><span className="were-farmers-purveyors-and-eaters-of-organically-grown-food-sub-1"></span><span className="were-farmers-purveyors-and-eaters-of-organically-grown-food-sub-0"></span><span className="were-farmers-purveyors-and-eaters-of-organically-grown-food-sub-1"></span><span></span>
+      </p>
+      <div className="button">
+        <span className="browse-our-shop">
+          Browse our shop
+        </span>
+      </div>
+      <div className="container-2">
+        <div className="jonathan-kemper-1-hhrd-io-lfp-uunsplash-1">
+        </div>
+        <div className="container-1">
+          <div className="stocksy-txp-226-f-62-b-2-ane-300-medium-45821931">
+          </div>
+          <p className="central-california-the-person-who-grew-these-was-located-in-central-california-and-er-hopefully-very-well-compensated">
+            <span className="central-california-the-person-who-grew-these-was-located-in-central-california-and-er-hopefully-very-well-compensated-sub-4"></span><span></span>
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="what-we-believe">
+          WHAT WE BELIEVE
+        </div>
+        <span className="we-believe-in-produce-tasty-produce-produce-like-apples-oranges-limes-lemons-guavas-carrots-cucumbers-jicamas-cauliflowers-brussels-sprouts-shallots-japanese-eggplants-asparagus-artichokes-jerusalem-artichokes-too-radishes-broccoli-baby-broccoli-broccolini-bok-choy-scallions-ginger-cherries-raspberries-cilantro-parsley-dill-what-are-we-forgetting-oh-onions-yams-avocados-lettuce-arugulato-somerocket-persian-cucumbers-in-addition-to-aforementionednormal-cucumbers-artichokes-zucchinis-pumpkins-squashwhat-some-cultures-call-pumpkins-sweet-potatoes-and-potato-potatoes-jackfruit-monk-fruit-fruit-of-the-loom-fruits-of-our-laborthis-website-sorrel-pineapple-mango-gooseberries-blackberries-tomatoes-heirloom-tomatoes-beets-chives-corn-endive-escarole-which-we-swear-were-vendors-of-organic-produce-but-if-you-asked-us-to-describe-what-escaroles-are">
+          We believe in produce. Tasty produce. Produce like:<br />
+          <br />
+          Apples. Oranges. Limes. Lemons. Guavas. Carrots. Cucumbers. Jicamas. Cauliflowers. Brussels sprouts. Shallots. Japanese eggplants. Asparagus. Artichokes—Jerusalem artichokes, too. Radishes. Broccoli. Baby broccoli. Broccolini. Bok choy. Scallions. Ginger. Cherries. Raspberries. Cilantro. Parsley. Dill. <br />
+          <br />
+          What are we forgetting?<br />
+          <br />
+          Oh! Onions. Yams. Avocados. Lettuce. Arugula (to some, “rocket”). Persian cucumbers, in addition to aforementioned “normal” cucumbers. Artichokes. Zucchinis. Pumpkins. Squash (what some cultures call pumpkins). Sweet potatoes and potato-potatoes. Jackfruit. Monk fruit. Fruit of the Loom. Fruits of our labor (this website). Sorrel. Pineapple. Mango. Gooseberries. Blackberries. Tomatoes. Heirloom tomatoes. Beets. Chives. Corn. Endive. Escarole, which, we swear, we’re vendors of organic produce, but if you asked us to describe what escaroles are...
+        </span>
+      </div>
+    </div>
+  )
+}
